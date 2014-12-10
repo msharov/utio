4692776888e112a53c7756c1da98781f9bcc3832
Mike Sharov <msharov@users.sourceforge.net>
2014-12-10 18:46:49 -0500

    Put compiled tests in the build directory

--- test/Module.mk
+++ test/Module.mk
@@ -1,9 +1,10 @@
 ################ Source files ##########################################
 
 test/SRCS	:= $(wildcard test/test?.cc)
-test/BVTS	:= $(test/SRCS:.cc=)
+test/BVTS	:= $(addprefix $O,$(test/SRCS:.cc=))
 test/OBJS	:= $(addprefix $O,$(test/SRCS:.cc=.o)) $Otest/stdmain.o
 test/DEPS	:= ${test/OBJS:.o=.d}
+test/OUTS	:= $(addprefix $O,$(test/SRCS:.cc=.out))
 ifdef BUILD_SHARED
 test/LIBS	:= -L$(abspath $O.) -l${NAME} -Wl,--rpath=$(abspath $O.)
 else
@@ -21,13 +22,13 @@ test/all:	${test/BVTS}
 # When the test runs, its output is compared to .std
 #
 test/run:	${test/BVTS}
-	@for i in ${test/BVTS}; do \
-	    echo "Running $$i"; \
-	    TERM=xterm ./$$i < $$i.cc &> $$i.out; \
-	    cmp $$i.std $$i.out && rm -f $$i.out; \
+	@for i in 0 1 2; do \
+	    echo "Running $Otest/test$$i"; \
+	    TERM=xterm $Otest/test$$i < test/test$$i.cc &> $Otest/test$$i.out; \
+	    cmp test/test$$i.std $Otest/test$$i.out && rm -f $Otest/test$$i.out; \
 	done
 
-${test/BVTS}: test/%: $Otest/%.o $Otest/stdmain.o ${ALLTGTS}
+${test/BVTS}: $Otest/%: $Otest/%.o $Otest/stdmain.o ${ALLTGTS}
 	@echo "Linking $@ ..."
 	@${LD} ${LDFLAGS} -o $@ $< $Otest/stdmain.o ${test/LIBS}
 
@@ -36,7 +37,7 @@ ${test/BVTS}: test/%: $Otest/%.o $Otest/stdmain.o ${ALLTGTS}
 clean:	test/clean
 test/clean:
 	@if [ -d $Otest ]; then\
-	    rm -f ${test/BVTS} ${test/OBJS} ${test/DEPS};\
+	    rm -f ${test/BVTS} ${test/OBJS} ${test/DEPS} ${test/OUTS};\
 	    rmdir $Otest;\
 	fi
 check:		test/run
